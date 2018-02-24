5308dcf1bf893b57754860ca8ab5d87d1fab3d16
mark <mpdbailey.technology@gmail.com>
2018-02-24 21:28:38 +0000

    Reads all variables from plc

--- app/src/main/java/com/pigdogbay/blackfins/model/fins/FinsLiveDataSource.kt
+++ app/src/main/java/com/pigdogbay/blackfins/model/fins/FinsLiveDataSource.kt
@@ -12,21 +12,74 @@ import java.io.InvalidObjectException
  */
 class FinsLiveDataSource(val finsSocket: FinsSocket) : ILiveDataSource{
 
+    //2 byte reponse, BCD 0.1°C
     private val finsCmdTemperature : FinsCommand = FinsCommandBuilder()
             .atMemoryAddress("258")
             .inMemoryArea(FinsMemoryArea.DMWord)
             .build()
+    //2 byte reponse, BCD 0.1°C
+    private val finsCmdTemperatureSetpoint : FinsCommand = FinsCommandBuilder()
+            .atMemoryAddress("280")
+            .inMemoryArea(FinsMemoryArea.DMWord)
+            .build()
+    //2 byte reponse, BCD 0-100%
+    private val finsCmdRelativeHumidity : FinsCommand = FinsCommandBuilder()
+            .atMemoryAddress("412")
+            .inMemoryArea(FinsMemoryArea.DMWord)
+            .build()
+    //2 byte reponse, BCD 0-100%
+    private val finsCmdRelativeHumiditySetpoint : FinsCommand = FinsCommandBuilder()
+            .atMemoryAddress("730")
+            .inMemoryArea(FinsMemoryArea.DMWord)
+            .build()
 
     override fun getLiveData(): LiveData {
+        val temperature = readTemperature()
+        val tempSetpoint = readTemperatureSetpoint()
+        val humidity = readRelativeHumidity()
+        val humiditySetpoint = readRelativeHumiditySetpoint()
+        return LiveData(temperature,tempSetpoint,humidity,humiditySetpoint)
+    }
+
+    private fun readTemperature() : Float {
         val data = toByteArray(finsCmdTemperature.FinsDatagram)
         finsSocket.send(data)
         val responseData = toIntArray(finsSocket.receive())
         if (!finsCmdTemperature.parseFinsResponse(responseData)){
-            throw InvalidObjectException("Unexpected response from the PLC")
+            throw InvalidObjectException("Unexpected response reading temperature from the PLC")
         }
-        var temperature = finsCmdTemperature.getResponseWord().toFloat()
         //Temperature in 0.1°C
-        temperature = temperature/10.0f
-        return LiveData(temperature,80f,0,0)
+        return finsCmdTemperature.getResponseWord().toFloat()/10f
+    }
+
+    private fun readTemperatureSetpoint() : Float {
+        val data = toByteArray(finsCmdTemperatureSetpoint.FinsDatagram)
+        finsSocket.send(data)
+        val responseData = toIntArray(finsSocket.receive())
+        if (!finsCmdTemperatureSetpoint.parseFinsResponse(responseData)){
+            throw InvalidObjectException("Unexpected response reading temperature setpoint from the PLC")
+        }
+        //Temperature Setpoint in 0.1°C
+        return finsCmdTemperatureSetpoint.getResponseWord().toFloat()/10f
+    }
+
+    private fun readRelativeHumidity() : Int {
+        val data = toByteArray(finsCmdRelativeHumidity.FinsDatagram)
+        finsSocket.send(data)
+        val responseData = toIntArray(finsSocket.receive())
+        if (!finsCmdRelativeHumidity.parseFinsResponse(responseData)){
+            throw InvalidObjectException("Unexpected response reading rel humidity from the PLC")
+        }
+        return finsCmdRelativeHumidity.getResponseWord()
+    }
+
+    private fun readRelativeHumiditySetpoint() : Int {
+        val data = toByteArray(finsCmdRelativeHumiditySetpoint.FinsDatagram)
+        finsSocket.send(data)
+        val responseData = toIntArray(finsSocket.receive())
+        if (!finsCmdRelativeHumiditySetpoint.parseFinsResponse(responseData)){
+            throw InvalidObjectException("Unexpected response reading rel humidity setpoint from the PLC")
+        }
+        return finsCmdRelativeHumiditySetpoint.getResponseWord()
     }
 }
